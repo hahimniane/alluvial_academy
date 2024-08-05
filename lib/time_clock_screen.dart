27028f394f60629b371053f552bd0c2761d838ec
Hashim Niane <68332405+hahimniane@users.noreply.github.com>
2024-08-05 19:59:09 -0400

    adding new things to the clock

--- lib/time_clock_screen.dart
+++ lib/time_clock_screen.dart
@@ -12,49 +12,137 @@ class _TimeClockScreenState extends State<TimeClockScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xffF6F6F6),
       body: Center(
-        child: MouseRegion(
-          onEnter: (_) => setState(() => _isHovered = true),
-          onExit: (_) => setState(() => _isHovered = false),
-          child: AnimatedContainer(
-            duration: Duration(milliseconds: 200),
-            transform: Matrix4.identity()..scale(_isHovered ? 1.2 : 1.0),
-            curve: Curves.easeInOut,
-            decoration: BoxDecoration(
-              shape: BoxShape.circle,
-              gradient: const LinearGradient(
-                colors: [Colors.blue, Color(0xff31C5DA)],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-              ),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.black.withOpacity(0.2),
-                  spreadRadius: _isHovered ? 4 : 2,
-                  blurRadius: _isHovered ? 10 : 5,
-                  offset: const Offset(0, 3), // changes position of shadow
-                ),
-              ],
-            ),
-            child: Container(
-              width: 120,
-              height: 120,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+        child: Column(
+          children: [
+            Container(
+              color: Colors.orange,
+              height: MediaQuery.of(context).size.height * 0.40,
+              child: Row(
+                crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Icon(
-                    Icons.timer_outlined,
-                    color: Colors.white,
-                    size: 40, // Adjust the size as needed
+                  Expanded(
+                    flex: 2,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Material(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(8),
+                        elevation: 4,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                "Today's clock",
+                                style: openSansHebrewTextStyle.copyWith(
+                                    fontWeight: FontWeight.bold,
+                                    color: Colors.black),
+                              ),
+                            ),
+                            Expanded(
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: Colors.white,
+                                ),
+                                padding: EdgeInsets.all(20),
+                                child: MouseRegion(
+                                  onEnter: (_) =>
+                                      setState(() => _isHovered = true),
+                                  onExit: (_) =>
+                                      setState(() => _isHovered = false),
+                                  child: AnimatedContainer(
+                                    duration: Duration(milliseconds: 200),
+                                    curve: Curves.easeInOut,
+                                    child: Transform.scale(
+                                      scale: _isHovered ? 1.2 : 1.0,
+                                      child: Container(
+                                        width: 150,
+                                        height: 150,
+                                        decoration: BoxDecoration(
+                                          shape: BoxShape.circle,
+                                          gradient: const LinearGradient(
+                                            colors: [
+                                              Colors.blue,
+                                              Color(0xff31C5DA)
+                                            ],
+                                            begin: Alignment.topCenter,
+                                            end: Alignment.bottomCenter,
+                                          ),
+                                          boxShadow: [
+                                            BoxShadow(
+                                              color:
+                                                  Colors.black.withOpacity(0.2),
+                                              spreadRadius: _isHovered ? 4 : 2,
+                                              blurRadius: _isHovered ? 10 : 5,
+                                              offset: const Offset(0,
+                                                  3), // changes position of shadow
+                                            ),
+                                          ],
+                                        ),
+                                        child: Column(
+                                          mainAxisAlignment:
+                                              MainAxisAlignment.center,
+                                          children: [
+                                            const Icon(
+                                              Icons.timer_outlined,
+                                              color: Colors.white,
+                                              size:
+                                                  30, // Adjust the size as needed
+                                            ),
+                                            const SizedBox(height: 5),
+                                            Text(
+                                              'Clock in',
+                                              style: openSansHebrewTextStyle
+                                                  .copyWith(
+                                                      color: Colors.white,
+                                                      fontSize: 12),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                            Card(
+                              elevation: 2,
+                              color: Color(0xffF5F5F5),
+                              child: Padding(
+                                padding: const EdgeInsets.all(3.0),
+                                child: Row(
+                                  children: [
+                                    Text("Total Work hours today:"),
+                                    Text(
+                                      "00:00",
+                                      style: openSansHebrewTextStyle.copyWith(
+                                          color: Colors.black,
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              ),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
                   ),
-                  SizedBox(height: 8),
-                  Text('Clock in',
-                      style: openSansHebrewTextStyle.copyWith(
-                          color: Colors.white)),
+                  Expanded(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.blue,
+                      child: Card(),
+                    ),
+                  ),
                 ],
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
