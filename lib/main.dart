f4868c527c19fc8985e7620ce3c39c4b559978cb
Hashim Niane <68332405+hahimniane@users.noreply.github.com>
2024-12-23 20:45:23 -0500

    made changes on the chat side bar and sending messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:alluwalacademyadmin/services.dart';
+import 'package:alluwalacademyadmin/Services/login_service.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
